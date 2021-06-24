174bf6a144bee97eaff5449481da7d3660df878f
brendaberzin <brenda.berzin@etec.sp.gov.br>
2021-06-24 00:25:50 -0300

    Intent implícita para o Instagram.

--- app/src/main/java/com/example/yogapad/help.java
+++ app/src/main/java/com/example/yogapad/help.java
@@ -3,6 +3,7 @@ package com.example.yogapad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,6 +20,14 @@ public class help extends AppCompatActivity {
     public void backToMenu (View view){
         Intent intent_back = new Intent(this, menu.class);
         startActivity(intent_back);
+    }
+
+    public void goToIG (View view){
+
+        Uri linkIG = Uri.parse("https://www.instagram.com/");
+        Intent intent_IG = new Intent(Intent.ACTION_VIEW, linkIG);
+        startActivity(intent_IG);
+
     }
 
 }
